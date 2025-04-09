f69e8c3d1b0cbb78d4c3b97d23cf649c3e7dce16
HichamBenbouzid <h.benbouzid@campus.unimib.it>
2025-04-09 20:02:53 +0200

    ADD: Added index.js

---
+++ flaskr/scripts/index.js
@@ -0,0 +1,28 @@
+//Controllo per caricare file .elf
+document.getElementById('fileInput').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const fileName = file.name;
+        if (!fileName.endsWith('.elf')) {
+            alert('You can only upload files with .elf extension!');
+            event.target.value = ''; // Resetta il campo file
+        } else {
+            window.location.href = 'https://github.com/aconsonni19/Binoculars'; //REINDIRIZZAMENTO PAGINA DI CONSO
+        }
+    }
+});
+
+//Funzione per il pulsante UPLOAD
+document.getElementById('uploadButton').addEventListener('click', function() {
+    document.getElementById('fileInput').click();
+});
+
+//Funzione per il pulsante ABOUT
+document.getElementById('aboutButton').addEventListener('click', function() {
+    window.location.href = '#about';
+});
+
+//Funzione per il pulsante GITHUB
+document.getElementById('githubButton').addEventListener('click', function() {
+    window.location.href = 'https://github.com/aconsonni19/Binoculars';
+});
