d44de5dcde9be510bb446b7343256b80b7121ae9
aconsonni19 <164369327+aconsonni19@users.noreply.github.com>
2025-04-10 23:47:54 +0200

    MOD: Modified the style and the script so that when the resizer is dragged, it changes color

--- flaskr/scripts/container_resize.js
+++ flaskr/scripts/container_resize.js
@@ -7,19 +7,19 @@ function initResizable(resizer, targetPane, isLeft) {
         startX = e.clientX;
         startWidth = targetPane.offsetWidth;
 
+        // Add the active class
+        resizer.classList.add("active");
+
         function onMouseMove(e) {
             const dx = e.clientX - startX;
             let newWidth;
 
             if (isLeft) {
-                // Left pane: grows/shrinks by moving the resizer to the right
                 newWidth = startWidth + dx;
             } else {
-                // Right pane: grows if you drag to the left shrinks if you drag right
-                newWidth = startWidth -dx;
+                newWidth = startWidth - dx;
             }
 
-            // Constrains the center panel to a minimum width
             if (newWidth > 100 && newWidth < window.innerWidth - 100) {
                 targetPane.style.width = `${newWidth}px`;
             }
@@ -28,6 +28,9 @@ function initResizable(resizer, targetPane, isLeft) {
         function onMouseUp() {
             document.removeEventListener("mousemove", onMouseMove);
             document.removeEventListener("mouseup", onMouseUp);
+
+            // Remove the active class
+            resizer.classList.remove("active");
         }
 
         document.addEventListener("mousemove", onMouseMove);
