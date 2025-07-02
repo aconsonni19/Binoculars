262ec3c5efa737974e20d763da2159cd27679192
aconsonni19 <164369327+aconsonni19@users.noreply.github.com>
2025-07-02 19:27:11 +0000

    DEL: Deleted unused approaches

--- src/TaintAnnotation.py
+++
@@ -1,7 +0,0 @@
-from claripy.ast.base import Annotation
-
-class TaintAnnotation(Annotation):
-    def __init__(self, tag):
-        self.tag = tag
-    def __str__(self):
-        return f"<{self.tag}>"
